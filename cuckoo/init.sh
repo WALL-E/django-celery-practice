deffcbf14db61ac2ad927e5736291f4ba0a4dfbc
zhangzheng <zhangzheng@qianbao.com>
2016-07-05 12:12:32 +0800

    添加说明init.sh

--- cuckoo/init.sh
+++ cuckoo/init.sh
@@ -13,3 +13,10 @@ chown celery:celery -R /apps/cuckoo
 killall -9 uwsgi
 uwsgi --ini uwsgi.ini --static-map /static=/usr/local/lib/python2.7/site-packages/django/contrib/admin/static
 
+echo "you need run commands:"
+echo "cat celery.conf >> /etc/supervisord.conf"
+echo "and"
+echo "./start.sh"
+echo ""
+echo "celery-admin port:10060"
+echo "celery-flwoer port:10070"
